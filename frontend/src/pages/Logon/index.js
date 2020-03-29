fcef5dc53989f63022c279b3d4dcd5c6b7fcd68f
TheAldo <47190980+TheAldo@users.noreply.github.com>
2020-03-29 00:35:56 -0300

    front-end app

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom'
+import api from '../../services/api';
+
 import './styles.css';
 import logImg from '../../assets/icon.svg';
 import logoImg from '../../assets/logo.svg';
@@ -6,6 +9,24 @@ import heroesImg from '../../assets/heroes.png';
 
 
 export default function Logon(){
+const [id, setId] = useState('');
+const history = useHistory();
+ async function handleLogin(e){
+e.preventDefault();
+
+try{
+ const response = await api.post('sessions', {id});
+ 
+ 
+  history.push('/profile');
+
+}catch(err){
+  alert('Falha no login, tente novamente');
+
+}
+
+
+ }
 
 
 return(
@@ -15,16 +36,20 @@ return(
 
      <img src={logoImg} alt="Be The Hero" />
 
-        <form>
+        <form onSubmit={handleLogin}>
         <h1>faça seu logon</h1>
 
-        <input placeholder="sua ID" />
+        <input 
+        placeholder="sua ID"
+        value={id}
+        onChange={e => setId(e.target.value)}
+        />
         <button className="button" type="submit">Entrar</button>
 
-        <a href="/register">
+        <Link className="back-link" to="/register">
             <img src={logImg} alt=""/>
           Não tenho cadastro
-        </a>
+        </Link>
 
 
         </form>
